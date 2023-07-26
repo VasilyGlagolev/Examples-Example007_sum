a07a0062291215403284ae02542a263a2b53de5f
VasilyGlagolev <139496040+VasilyGlagolev@users.noreply.github.com>
2023-07-26 22:34:40 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите число: ");
+        int num = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Чётные числа от 1 до " + num + ":");
+
+        for (int i = 2; i <= num; i += 2)
+        {
+            Console.Write(i + " ");
+        }
+
+        Console.ReadKey();
+    }
+}
